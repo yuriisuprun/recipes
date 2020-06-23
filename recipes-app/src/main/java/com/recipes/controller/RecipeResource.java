9b78440719e23f23168453ab4b3213bc2a93a8df
Yurii_Suprun <Yurii_Suprun@gmail.com>
2020-06-23 10:57:28 +0300

    AddingRestAPI: Adds createRecipe() method

--- recipes-app/src/main/java/com/recipes/controller/RecipeResource.java
+++ recipes-app/src/main/java/com/recipes/controller/RecipeResource.java
@@ -1,9 +1,12 @@
 package com.recipes.controller;
 
+import com.recipes.model.Recipe;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -20,6 +23,14 @@ public class RecipeResource {
     public Mono<String> getRecipeById(@PathVariable String recipeId){
 
 
-        return Mono.just("{}");
+        return Mono.just("{Recipe}");
+    }
+
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<String> createRecipe(@RequestBody Mono<Recipe> recipe){
+
+
+        return Mono.just("{Recipe}");
     }
 }
