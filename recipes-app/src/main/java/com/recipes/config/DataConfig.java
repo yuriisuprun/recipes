5b6054fd594ecd61d518215adebc5e6dedecebc8
Yurii_Suprun <Yurii_Suprun@gmail.com>
2020-06-28 17:26:30 +0300

    Changes DataConfig

--- recipes-app/src/main/java/com/recipes/config/DataConfig.java
+++ recipes-app/src/main/java/com/recipes/config/DataConfig.java
@@ -1,9 +1,15 @@
 package com.recipes.config;
 
+import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
 /**
  * Created by Yurii_Suprun
@@ -13,5 +19,15 @@ import org.springframework.context.annotation.Profile;
 @Import(EmbeddedMongoAutoConfiguration.class)
 public class DataConfig {
 
+    public static final String DATABASE_NAME = "recipes";
 
+    @Bean
+    public ReactiveMongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient){
+        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
+    }
+
+    @Bean
+    public ReactiveMongoOperations reactiveMongoTemplate(ReactiveMongoDatabaseFactory mongoDatabaseFactory){
+        return new ReactiveMongoTemplate(mongoDatabaseFactory);
+    }
 }
