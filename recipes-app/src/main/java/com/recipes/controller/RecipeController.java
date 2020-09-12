3f0f9672682b0fdd52e8cde1c9e24b328f19f4dc
Yurii_Suprun <Yurii_Suprun@gmail.com>
2020-09-12 11:08:07 +0300

    Adds changes to RecipeController

--- recipes-app/src/main/java/com/recipes/controller/RecipeController.java
+++ recipes-app/src/main/java/com/recipes/controller/RecipeController.java
@@ -1,6 +1,8 @@
 package com.recipes.controller;
 
 import com.recipes.model.Recipe;
+import com.recipes.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,43 +13,55 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
  * Created by Yurii_Suprun
  */
-@RestController()
-@RequestMapping("v1/recipe")
+@RestController
+@RequestMapping(RecipeController.V1_RECIPE_URI)
 @CrossOrigin
-public class RecipeResource {
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public static final String V1_RECIPE_URI = "v1/recipe/";
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping(path = "{recipeId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Mono<String> getRecipeById(@PathVariable String recipeId){
+    public Mono<Recipe> getRecipeById(@PathVariable String recipeId){
 
+        return recipeService.getRecipe(recipeId);
+    }
 
-        return Mono.just("{Recipe}");
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Flux<Recipe> getRecipes(){
+
+        return recipeService.listAllRecipes();
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Mono<String> createRecipe(@RequestBody Mono<Recipe> recipe){
+    public Mono<Recipe> createRecipe(@RequestBody Mono<Recipe> recipe){
 
-
-        return Mono.just("{Recipe}");
+        return recipeService.createRecipe(recipe);
     }
 
     @PutMapping(path = "recipeId", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
              consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Mono<String> updateDescription(@PathVariable String recipeId, @RequestBody Mono<Recipe> recipe){
+    public Mono<Recipe> updateDescription(@PathVariable String recipeId, @RequestBody Mono<Recipe> recipe){
 
-
-        return Mono.just("{Recipe}");
+        return recipeService.updateRecipe(recipeId, recipe);
     }
 
     @DeleteMapping(path = "{recipeId}")
     public Mono<Boolean> deleteRecipe(@PathVariable String recipeId){
 
-
-        return Mono.just(true);
+        return recipeService.deleteRecipe(recipeId);
     }
 }
