f08c8df016959c37ecdffe8aeb7e6e14061592e8
Yurii_Suprun <Yurii_Suprun@gmail.com>
2020-09-12 11:01:26 +0300

    Changes ApiConfig

--- recipes-app/src/main/java/com/recipes/config/ApiConfig.java
+++ recipes-app/src/main/java/com/recipes/config/ApiConfig.java
@@ -7,7 +7,12 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.Collections;
 
 /**
  * Created by Yurii_Suprun
@@ -17,7 +22,25 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 public class ApiConfig {
 
     @Bean
-    public ObjectMapper objectMapper(){
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
@@ -26,7 +49,7 @@ public class ApiConfig {
     }
 
     @Bean
-    public ObjectWriter objectWriter(ObjectMapper objectMapper){
+    public ObjectWriter objectWriter(ObjectMapper objectMapper) {
         return objectMapper.writerWithDefaultPrettyPrinter();
     }
 }
